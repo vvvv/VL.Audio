01b728d54b234ecc59478478a9254aa6a759ba7d
joreg <joreg@vvvv.org>
2019-11-13 18:39:42 +0100

    init

---
+++ src/AudioDeviceEnum.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using VL.Lib;
+using VL.Lib.Collections;
+using NAudio.Wave;
+
+namespace VL.Naudio
+{
+    [Serializable]
+    public class AsioInputDevice : DynamicEnumBase<AsioInputDevice, AsioInputDeviceDefinition>
+    {
+        public AsioInputDevice(string value) : base(value)
+        {
+        }
+
+        //this method needs to be imported in VL to set the default
+        public static AsioInputDevice CreateDefault()
+        {
+            //use method of base class if nothing special required
+            return CreateDefaultBase();
+        }
+    }
+
+    public class AsioInputDeviceDefinition : DynamicEnumDefinitionBase<AsioInputDeviceDefinition>
+    {
+        protected override IObservable<object> GetEntriesChangedObservable()
+        {
+            return HardwareChangedEvents.HardwareChanged;
+        }
+
+        protected override IReadOnlyDictionary<string, object> GetEntries()
+        {
+            Dictionary<string, object> driverNames = new Dictionary<string, object>();
+
+            foreach (var driverName in AsioOut.GetDriverNames())
+            {
+                //the return dictionary holds the names of the entries as key with an optional "tag"
+                //here the tag is null but you can provide any object that you want to associate with the entry
+                driverNames[driverName] = null;
+            }
+
+            return driverNames;
+        }
+    }
+}
