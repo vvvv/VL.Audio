bf7f7f98be8e9b0861e08b730b00c674f9b52dd9
tebjan <tebjan@vvvv.org>
2020-12-21 23:58:33 +0100

    Using something from the VVVV.Utils.dll to copy it to the output

---
+++ src/UseDllHack.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace VL.Audio
+{
+    public class UseDllHack
+    {
+        // this makes sure the VVVV.Utils.dll gets copied to the output when exported from vvvv gamma
+        public static double IgnoreMe() => VVVV.Utils.VMath.VMath.Random.NextDouble();
+
+    }
+}
