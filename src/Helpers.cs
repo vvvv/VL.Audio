6ac3e277189c71275203036c1e8096a283ffaa66
joreg <joreg@vvvv.org>
2022-01-19 21:17:40 +0100

    added BufferReader and reworked WaveForm

--- src/Helpers.cs
+++ src/Helpers.cs
@@ -8,9 +8,9 @@ using VVVV.Audio;
 
 namespace VL.Audio
 {
-    public static class WaveformHelper
+    public static class Helpers
     {
-        public static void ReadIntoSpread(CancellationToken ct, AudioFileReaderVVVV audiofile, bool loop, double startTime, double endTime, bool toMono, int peakCount, float minValue, out double[][]outputBuffers)
+        public static void ReadWaveForm(CancellationToken ct, AudioFileReaderVVVV audiofile, bool loop, float startTime, float endTime, bool toMono, int peakCount, float minValue, out float[][]outputBuffers)
         {
             var channels = audiofile.WaveFormat.Channels;
             long samples = (long)Math.Round(audiofile.TotalTime.TotalSeconds * audiofile.WaveFormat.SampleRate);
@@ -23,16 +23,14 @@ namespace VL.Audio
             var localSpreadCount = (int)Math.Min(peakCount, samples);
             if (toMono)
             {
-                outputBuffers = new double[1][];
-                outputBuffers[0] = new double[localSpreadCount];
+                outputBuffers = new float[1][];
+                outputBuffers[0] = new float[localSpreadCount];
             }
             else
             {
-                outputBuffers = new double[channels][];
+                outputBuffers = new float[channels][];
                 for (int i = 0; i < channels; i++)
-                {
-                    outputBuffers[i] = new double[localSpreadCount];
-                }
+                    outputBuffers[i] = new float[localSpreadCount];
             }
 
             int blockSize = (int)(samples / localSpreadCount);
@@ -49,9 +47,7 @@ namespace VL.Audio
                 {
                     maxValue = minValue;
                     for (int i = 0; i < samplesRead; i += channels)
-                    {
                         maxValue = Math.Max(maxValue, Math.Abs(buffer[i + channel]));
-                    }
 
                     if (toMono)
                     {
@@ -61,6 +57,34 @@ namespace VL.Audio
                     {
                         outputBuffers[channel][slice] = maxValue;
                     }
+                    ct.ThrowIfCancellationRequested();
+                }
+            }
+        }
+
+        public static void ReadBuffer(CancellationToken ct, AudioFileReaderVVVV audiofile, out float[][] outputBuffers)
+        {
+            var channels = audiofile.WaveFormat.Channels;
+            long samples = (long)Math.Round(audiofile.TotalTime.TotalSeconds * audiofile.WaveFormat.SampleRate);
+
+            outputBuffers = new float[channels][];
+            for (int i = 0; i < channels; i++)
+                outputBuffers[i] = new float[samples];
+
+            int blockSize = (int)Math.Min(int.MaxValue / channels, samples);
+            var bufferSize = blockSize * channels;
+            var buffer = new float[bufferSize];
+            var takes = (samples * channels) / bufferSize;
+            for (int t = 0; t < takes; t++)
+            {
+                //read one interleaved block
+                var samplesRead = audiofile.Read(buffer, 0, bufferSize);
+                //split into channels
+                for (int channel = 0; channel < channels; channel++)
+                {
+                    for (int i = 0; i < samplesRead; i += channels)
+                        outputBuffers[channel][t * blockSize + i] = buffer[i + channel];
+
                     ct.ThrowIfCancellationRequested();
                 }
             }
