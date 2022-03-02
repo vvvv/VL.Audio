4ed5c472c8d74a9a50acc513e27c7aedf8999539
joreg <joreg@vvvv.org>
2022-03-02 16:23:27 +0100

    prevents AV with SingleSignal

--- src/Core/MultiChannelSignal.cs
+++ src/Core/MultiChannelSignal.cs
@@ -25,8 +25,11 @@ namespace VL.Audio
         
         protected override void FillBuffer(float[] buffer, int offset, int count)
         {
-            FRequestBufferFill(offset, count);
-            Array.Copy(FBuffer, offset, buffer, offset, count);
+            if (FRequestBufferFill != null)
+            {
+                FRequestBufferFill(offset, count);
+                Array.Copy(FBuffer, offset, buffer, offset, count);
+            }
         }
         
         public override void Dispose()
