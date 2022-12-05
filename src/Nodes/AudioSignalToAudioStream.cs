86cb3a58212a9c8290defd84f3ea7c7837efe196
Elias Holzer <elias@vvvv.org>
2022-12-05 13:09:19 +0100

    Moves the project to net6 and adds AudioSourceToAudioSignal & AudioSignalToAudioStream (needed by NDI)

---
+++ src/Nodes/AudioSignalToAudioStream.cs
@@ -0,0 +1,53 @@
+﻿using CommunityToolkit.HighPerformance;
+using CommunityToolkit.HighPerformance.Buffers;
+using System;
+using System.Collections.Generic;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using VL.Core;
+using VL.Lib.Basics.Audio;
+using VL.Lib.Basics.Resources;
+
+namespace VL.Audio.Nodes
+{
+    public sealed class AudioSignalToAudioStream : MultiChannelInputSignal
+    {
+        private readonly Subject<AudioFrame<float>> _audioStream = new Subject<AudioFrame<float>>();
+        private readonly BehaviorSubject<AudioFrame<float>> _silentAudioStream = new BehaviorSubject<AudioFrame<float>>(AudioFrame<float>.Empty);
+
+        private string _metadata;
+
+        public IReadOnlyList<AudioSignal> Update(IReadOnlyList<AudioSignal> input, string metadata, out IObservable<AudioFrame<float>> audioStream)
+        {
+            if (input.Count != FOutputCount)
+            {
+                SetOutputCount(input.Count);
+                FNeedsRead = true;
+            }
+
+            Input = input;
+            _metadata = metadata;
+
+            audioStream = input.Count > 0 ? _audioStream : _silentAudioStream;
+
+            return Outputs;
+        }
+
+        protected override void FillBuffers(float[][] buffers, int offset, int sampleCount)
+        {
+            var inputs = Input;
+
+            using var memoryOwner = MemoryOwner<float>.Allocate(buffers.Length * sampleCount);
+            var memory = memoryOwner.Memory.AsMemory2D(buffers.Length, sampleCount);
+            var dst = memory.Span;
+            for (int i = 0; i < buffers.Length; i++)
+            {
+                inputs[i].Read(buffers[i], offset, sampleCount);
+                var src = new Span<float>(buffers[i], offset, sampleCount);
+                src.CopyTo(dst.GetRowSpan(i));
+            }
+
+            _audioStream.OnNext(new AudioFrame<float>(memory, SampleRate, IsInterleaved: false, Metadata: _metadata));
+        }
+    }
+}
---
+++ src/Nodes/AudioSourceToAudioSignal.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VL.Core;
+using VL.Lib.Basics.Audio;
+
+namespace VL.Audio.Nodes
+{
+    public sealed class AudioSourceToAudioSignal : MultiChannelSignal
+    {
+        private AudioSignal[] _outputs;
+        private IAudioSource<float> _audioSource;
+        private Optional<int> _channelCount;
+
+        public AudioSourceToAudioSignal()
+        {
+            _outputs = Outputs.ToArray();
+        }
+
+        public IReadOnlyList<AudioSignal> Update(IAudioSource<float> audioSource, Optional<int> channelCount)
+        {
+            _audioSource = audioSource;
+            _channelCount = channelCount;
+            return _outputs;
+        }
+
+        protected override void FillBuffers(float[][] buffers, int offset, int sampleCount)
+        {
+            var channelCount = _channelCount.HasValue ? _channelCount.Value : 0;
+
+            using var audioFrameHandle = _audioSource?.GrabAudioFrame(sampleCount, SampleRate, channelCount, interleaved: false);
+            var audioFrame = audioFrameHandle?.Resource;
+
+            if (audioFrame is null)
+            {
+                foreach (var buffer in buffers)
+                    buffer.ReadSilence(offset, sampleCount);
+            }
+            else
+            {
+                if (audioFrame.ChannelCount != FOutputCount)
+                {
+                    SetOutputCount(audioFrame.ChannelCount);
+                    ManageBuffers(sampleCount);
+                    buffers = FReadBuffers;
+                    _outputs = Outputs.ToArray();
+                }
+
+                for (int i = 0; i < buffers.Length; i++)
+                    audioFrame.CopyChannelTo(i, new Span<float>(buffers[i], offset, sampleCount));
+            }
+        }
+    }
+}
